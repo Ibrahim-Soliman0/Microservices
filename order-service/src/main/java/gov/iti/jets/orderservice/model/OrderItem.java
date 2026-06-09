04802b709b3b944f7c25e16d7ff5475d0f90a8e7
adhamkhaled312 <105177075+adhamkhaled312@users.noreply.github.com>
2026-06-09 16:27:00 +0300

    add order-service project with model and repository layers

---
+++ order-service/src/main/java/gov/iti/jets/orderservice/model/OrderItem.java
@@ -0,0 +1,34 @@
+package gov.iti.jets.orderservice.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @Column(nullable = false)
+    private Long productId;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private Double unitPrice;
+}
