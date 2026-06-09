3cfd6f6df5283160ffe1f8dad6d2a601c62b8142
Mostafa Abdelfatah Elwishy <mostafaelweshy777@gmail.com>
2026-06-10 00:24:43 +0300

    adde env

--- oauth2/src/main/java/com/example/oauth2/Oauth2Application.java
+++ oauth2/src/main/java/com/example/oauth2/Oauth2Application.java
@@ -1,5 +1,6 @@
 package com.example.oauth2;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Oauth2Application {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
 		SpringApplication.run(Oauth2Application.class, args);
 	}
 
