14b7c3dbdef63547abf9913d8a5deac693cb9c4f
adhamkhaled312 <105177075+adhamkhaled312@users.noreply.github.com>
2026-06-09 17:43:27 +0300

    add rabbit mq configurations

---
+++ order-service/src/main/java/gov/iti/jets/orderservice/config/RabbitMQConfig.java
@@ -0,0 +1,51 @@
+package gov.iti.jets.orderservice.config;
+
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${rabbitmq.queue}")
+    private String queue;
+
+    @Value("${rabbitmq.routing-key}")
+    private String routingKey;
+
+    @Bean
+    public DirectExchange exchange(){
+        return new DirectExchange(exchange);
+    }
+
+    @Bean
+    public Queue queue(){
+        return new Queue(queue);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange){
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
+        return template;
+    }
+}
